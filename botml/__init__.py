0f2c1988b0b26927a6db9a38da43fb340d8e2f94
ligarius <ligarius@gmail.com>
2025-06-29 18:55:35 -0400

    Add walk-forward optimization module and tests

--- botml/__init__.py
+++ botml/__init__.py
@@ -4,6 +4,7 @@ from .features import add_features
 from .labeling import create_labels
 from .risk import PositionSizer, RiskManager
 from .utils import load_config, setup_logging
+from .walkforward import rolling_windows, walk_forward_optimize
 
 __all__ = [
     "add_features",
@@ -12,4 +13,6 @@ __all__ = [
     "RiskManager",
     "load_config",
     "setup_logging",
+    "rolling_windows",
+    "walk_forward_optimize",
 ]
