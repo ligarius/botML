ad272776eefffc773fbc364d8c73bae53a0654f9
ligarius <ligarius@gmail.com>
2025-06-29 22:29:15 -0400

    refactor: move single-pair Backtester into package

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,12 +1,14 @@
 """Backtesting package providing portfolio utilities and engine."""
 
 from .engine import MultiPairBacktester
+from .simple import Backtester
 from .portfolio import Portfolio
 from .trade import Trade
 from .metrics import compute_metrics
 from .walkforward import rolling_windows, walk_forward
 
 __all__ = [
+    "Backtester",
     "MultiPairBacktester",
     "Portfolio",
     "Trade",
